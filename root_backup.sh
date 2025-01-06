24747f039f3a39776bbfde450d3e2593fec7f8d8
ccasper <ccasper@gmail.com>
2025-01-05 20:49:25 -0600

    Update root_backup.sh

--- root_backup.sh
+++ root_backup.sh
@@ -5,15 +5,38 @@
 #
 # We include / /home /boot since these are sometimes separate file systems.
 
-OUTPUT=$HOME/root_backup.tgz
-tar -cvpzf $OUTPUT --one-file-system \
+OUTPUT=/backup.tgz
+
+tar -cvpzf -C / $OUTPUT \
+ --one-file-system \
  --exclude=$OUTPUT \
- --exclude=/backup \
- --exclude=/var/cache/* \
- --exclude=/var/log/* \
- --exclude=/var/backups/* \
- --exclude=/home/*/.bash_history \
- --exclude=/tmp/* \
- --exclude=/var/www/*/log \
-/ /home /boot
+ --exclude=/proc \
+ --exclude=/tmp \
+ --exclude=/srv \
+ --exclude=/mnt/*/ \
+ --exclude=/dev \
+ --exclude=/var/lib/plexmediaserver \
+ --exclude=/opt/plexmediaserver \
+ --exclude=/opt/getmail6 \
+ --exclude=/sys \
+ --exclude=/run \
+ --exclude=/media \
+ --exclude=/var/log \
+ --exclude=/var/cache/apt/archives \
+ --exclude=/usr/src/linux-headers* \
+ --exclude=/home/*/.gvfs \
+ --exclude=/home/*/Downloads \
+ --exclude=/home/*/.cache \
+ --exclude=/home/amcrest/*/* \
+ --exclude=/home/*/.local/share/Trash / /boot /home
+
+## To Restore:
+# tar -xvpzf backup.tar.gz -C /media/newroot --numeric-owner
+# mkdir /media/newroot/proc /media/newroot/sys /media/newroot/mnt /media/newroot/media
+# sudo -s for f in dev dev/pts proc ; do mount --bind /$f /media/newroot/$f ; done
+# chroot /media/newroot
+# dpkg-reconfigure grub-pc
+## Rebuild /dev (which is done automatically on restart)
+# udevadm control --reload-rules
+# udevadm trigger
 
